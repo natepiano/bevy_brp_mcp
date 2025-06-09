07a520231be063fd89087b8e11f2738169504744
natepiano <slicks.curable.0k@icloud.com>
2025-06-09 23:02:49 +0200

    Rename help_text_files directory to help_text
    - Add include_help_text\! macro to centralize directory path
    - Update all tool description includes to use the macro
    - Makes directory name easier to change in the future

--- src/constants.rs
+++ src/constants.rs
@@ -2,7 +2,14 @@ pub const PROFILE_DEBUG: &str = "debug";
 pub const PROFILE_RELEASE: &str = "release";
 pub const DEFAULT_PROFILE: &str = PROFILE_DEBUG;
 
+// Macro to include help text files
+macro_rules! include_help_text {
+    ($file:expr) => {
+        include_str!(concat!("../help_text/", $file))
+    };
+}
+
 // Tool descriptions
-pub const LIST_BEVY_APPS_DESC: &str = include_str!("../help_text_files/tools/list_bevy_apps.txt");
-pub const LIST_BEVY_EXAMPLES_DESC: &str = include_str!("../help_text_files/tools/list_bevy_examples.txt");
-pub const LAUNCH_BEVY_APP_DESC: &str = include_str!("../help_text_files/tools/launch_bevy_app.txt");
+pub const LIST_BEVY_APPS_DESC: &str = include_help_text!("tools/list_bevy_apps.txt");
+pub const LIST_BEVY_EXAMPLES_DESC: &str = include_help_text!("tools/list_bevy_examples.txt");
+pub const LAUNCH_BEVY_APP_DESC: &str = include_help_text!("tools/launch_bevy_app.txt");
